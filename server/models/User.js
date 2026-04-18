eb1ecee1feac56225b061d00baf02eee0917b888
Aaron Russell <cpl.adrusselljr@gmail.com>
2026-04-18 16:24:59 -0400

    userSchema created

---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    // =========================
+    // COMPANY RELATIONSHIP
+    // =========================
+    companyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+    },
+
+    // =========================
+    // BASIC INFO
+    // =========================
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { tuep: String, required: true, unique: true, lowercase: true, trim: true },
+
+    // =========================
+    // ROLE SYSTEM
+    // =========================
+    role: {
+        type: String,
+        enum: ['admin', 'pm', 'foreman', 'tech'],
+        default: 'tech'
+    },
+    // =========================
+    // ACCOUNT STATUS
+    // =========================
+    isActive: { type: Boolean, default: true },
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userSchema)
